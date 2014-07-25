bc5984fb58a30c51d1734091288eef3d2637cc02
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2014-07-25 12:03:54 +0200

    enhance unsafe functions
    Add Led.unsafe_light(), LedSet.unsafe_buffer(), LedSet.unsafe_set() and
    LedSet.unsafe_light().
    
    Use the unsafe functions to reduce code duplication and improve
    factorization.

--- piglow_spark/components/led_set.py
+++ piglow_spark/components/led_set.py
@@ -12,38 +12,45 @@ class LedSet(object):
 
     def buffer(self, brightness):
         """Buffer a brightness value."""
-        if self.__leds and Led.check_brightness(brightness):
-            for led in self.__leds:
-                led.unsafe_buffer(brightness)
+        if Led.check_brightness(brightness):
+            self.unsafe_buffer(brightness)
 
     def light(self, brightness):
         """Immediately light the set of LEDs."""
         if self.__leds and Led.check_brightness(brightness):
-            for led in self.__leds:
-                led.unsafe_set(brightness)
-            self.__bus.update()
+            self.unsafe_light(brightness)
 
     def gamma_buffer(self, brightness):
         """Buffer a gamma corrected brightness value."""
         if self.__leds:
             correct_brightness = Led.gamma_correct(brightness)
-            for led in self.__leds:
-                led.unsafe_buffer(correct_brightness)
+            self.unsafe_buffer(correct_brightness)
 
     def gamma_light(self, brightness):
         """Immediately light the set of LEDs."""
         if self.__leds:
             correct_brightness = Led.gamma_correct(brightness)
-            for led in self.__leds:
-                led.unsafe_set(correct_brightness)
-            self.__bus.update()
+            self.unsafe_light(correct_brightness)
+
+    def unsafe_buffer(self, brightness):
+        """(Unsafe) Buffer a brightness value."""
+        for led in self.__leds:
+            led.unsafe_buffer(brightness)
+
+    def unsafe_set(self, brightness):
+        """(Unsafe) Set the LED set's brightness."""
+        for led in self.__leds:
+            led.unsafe_set(brightness)
+
+    def unsafe_light(self, brightness):
+        """(Unsafe) Immediately light the LED set."""
+        self.unsafe_set(brightness)
+        self.__bus.update()
 
     def off(self):
         """Switch off the set of LEDs."""
         if self.__leds:
-            for led in self.__leds:
-                led.unsafe_set(0)
-            self.__bus.update()
+            self.unsafe_light(0)
 
     def number(self):
         """Number of LEDs in the set."""
