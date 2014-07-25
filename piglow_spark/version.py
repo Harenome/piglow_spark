51ba640bba20f60c272b976d019456750eec9da5
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2014-07-25 03:05:00 +0200

    bump v0.2.0

--- piglow_spark/version.py
+++ piglow_spark/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """PiGlow Spark version."""
 
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 2, 0)
 __pre_release_label__ = ""
 __version__ = '.'.join(map(str, __version_info__)) + __pre_release_label__
